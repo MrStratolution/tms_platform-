6e02362b0aa22a3fd49177fd29c5fdc60e90a01f
vishal0573 <vishal@codexaureus.com>
2026-04-09 16:38:06 +0530

    added dynamic

--- src/app/(admin)/console/layout.tsx
+++ src/app/(admin)/console/layout.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from 'react'
 
+export const dynamic = 'force-dynamic'
+
 export default function ConsoleSegmentLayout(props: { children: ReactNode }) {
   const { children } = props
   return children
